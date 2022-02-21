f1db1f9c4ecdfac52c8cfcd27c3a529fa9e22c0b
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-02-21 17:28:09 +0500

    Create page1_abu.py

---
+++ page1_abu.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+page = requests.get('https://www.goodreads.com/list/show/1.Best_Books_Ever')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+ori = []
+ser = []
+gen = []
+
+#----------------------------------------------------------------------#
+#Original publish year
+
