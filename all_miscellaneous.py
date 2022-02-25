1789a9c60ad5e552375ea2b9d5982de7be316c63
peterzorve <98400766+peterzorve@users.noreply.github.com>
2022-02-25 17:44:21 +0200

    all_functions

---
+++ all_miscellaneous.py
@@ -0,0 +1,29 @@
+
+
+# THINGS TO SCRAP  
+        #   1. TITLE            
+        #   2. AUTHORS          
+        #   3. RATINGS     
+        #   4. REVIEWS       
+        #   5. PAGES         
+        #   6. AWARDS  
+        #   7. ORIGINAL YEAR    
+        #   8. SERIES           
+        #   9. GENRES          
+        #   10. PLACES                  
+        #   11. URL LINKS
+
+
+
+###################     FRONTPAGE LINKS   ###################################################################################################
+# url_1 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever'
+# url_2 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=2'
+# url_3 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=3'
+# url_4 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=4'
+# url_5 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=5'
+# url_6 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=6'
+# url_7 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=7'
+# url_8 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=8'
+# url_9 =  'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=9'
+# url_10 = 'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=10'
+# url_11 = 'https://www.goodreads.com/list/show/1.Best_Books_Ever?page=11'
